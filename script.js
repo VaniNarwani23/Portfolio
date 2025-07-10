8785b41444f9f59227641ce903e8490c1e2154e9
Vani <vaninarwani@gmail.com>
2025-07-10 20:16:08 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+
 const toggle = document.getElementById("modeToggle");
 const emoji = document.querySelector(".emoji");
 
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode");
   emoji.textContent = toggle.checked ? "🌞" : "🌙";
+});
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
